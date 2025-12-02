43650aaf51868c20be038d41e4e9c2018c77f13f
KNR1997 <kethakaranasinghe@gmail.com>
2025-12-02 11:14:26 +0530

    refactor: change validation field to payment_month

--- src/components/enrollment-payments/enrollment-payment-validation-schema.ts
+++ src/components/enrollment-payments/enrollment-payment-validation-schema.ts
@@ -3,5 +3,5 @@ import * as yup from 'yup';
 export const enrollmentPaymentValidationSchema = yup.object().shape({
   student: yup.object().required('form:error-student-required'),
   course: yup.object().required('form:error-course-required'),
-  month: yup.object().required('form:error-month-required'),
+  payment_month: yup.object().required('form:error-month-required'),
 });
