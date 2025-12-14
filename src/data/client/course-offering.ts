4c9159c8740082225978bab2052dd7c21fe0ef3d
KNR1997 <kethakaranasinghe@gmail.com>
2025-12-14 14:44:43 +0530

    feat: add course offering API client and update API endpoints for course offerings and user enrollments

---
+++ src/data/client/course-offering.ts
@@ -0,0 +1,24 @@
+import {
+  CourseOffering,
+  CourseOfferingPaginator,
+  CourseOfferingQueryOptions,
+  CreateCourseOfferingInput,
+  QueryOptions,
+} from '@/types';
+import { API_ENDPOINTS } from './api-endpoints';
+import { crudFactory } from './curd-factory';
+import { HttpClient } from './http-client';
+
+export const courseOfferingClient = {
+  ...crudFactory<CourseOffering, QueryOptions, CreateCourseOfferingInput>(
+    API_ENDPOINTS.COURSE_OFFERING,
+  ),
+  paginated: ({ name, ...params }: Partial<CourseOfferingQueryOptions>) => {
+    return HttpClient.get<CourseOfferingPaginator>(API_ENDPOINTS.COURSE_OFFERING, {
+      searchJoin: 'and',
+      self,
+      ...params,
+      search: HttpClient.formatSearchParams({ name }),
+    });
+  },
+};
