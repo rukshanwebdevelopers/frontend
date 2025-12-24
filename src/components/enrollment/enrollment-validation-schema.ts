80aede4a78bed779076c9b84a08edc2107a961fe
KNR1997 <kethakaranasinghe@gmail.com>
2025-12-24 20:28:09 +0530

    fix: update course property to courseOffering in enrollmentValidationSchema

--- src/components/enrollment/enrollment-validation-schema.ts
+++ src/components/enrollment/enrollment-validation-schema.ts
@@ -2,5 +2,5 @@ import * as yup from 'yup';
 
 export const enrollmentValidationSchema = yup.object().shape({
   student: yup.object().required('form:error-student-required'),
-  course: yup.object().required('form:error-course-required'),
+  courseOffering: yup.object().required('form:error-course-required'),
 });
