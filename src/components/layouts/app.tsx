83885899d96ed57f4d53fcf12e148f6e7d0e8c66
KNR1997 <kethakaranasinghe@gmail.com>
2026-01-26 16:06:21 +0530

    feat: AppLayout -> Teacher permission

--- src/components/layouts/app.tsx
+++ src/components/layouts/app.tsx
@@ -1,7 +1,8 @@
-import { SUPER_ADMIN } from '@/utils/constants';
+import { SUPER_ADMIN, TEACHER } from '@/utils/constants';
 import dynamic from 'next/dynamic';
 
 const AdminLayout = dynamic(() => import('@/components/layouts/admin'));
+const TeacherLayout = dynamic(() => import('@/components/layouts/teacher'));
 const StudentLayout = dynamic(() => import('@/components/layouts/student'));
 
 export default function AppLayout({
@@ -12,7 +13,10 @@ export default function AppLayout({
 }) {
   if (userPermissions?.includes(SUPER_ADMIN)) {
     return <AdminLayout {...props} />;
+  } else if (userPermissions?.includes(TEACHER)) {
+    return <TeacherLayout {...props} />;
+  } else {
+    return <StudentLayout {...props} />;
   }
-  return <StudentLayout {...props} />;
   // return <OwnerLayout {...props} />;
 }
