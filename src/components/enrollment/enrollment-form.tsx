f066cbc8d07f5131f7ea38b084e125cbdfbcbd2a
KNR1997 <kethakaranasinghe@gmail.com>
2026-01-28 12:04:06 +0530

    chore: better import structure

--- src/components/enrollment/enrollment-form.tsx
+++ src/components/enrollment/enrollment-form.tsx
@@ -1,24 +1,28 @@
-import { Control, FieldErrors, useForm } from 'react-hook-form';
-import Button from '@/components/ui/button';
-import Card from '@/components/common/card';
-import Description from '@/components/ui/description';
 import { useRouter } from 'next/router';
-import { CourseOffering, Enrollment, Student } from '@/types';
-import { useTranslation } from 'next-i18next';
-import { yupResolver } from '@hookform/resolvers/yup';
-import { useSettingsQuery } from '@/data/settings';
-import StickyFooterPanel from '@/components/ui/sticky-footer-panel';
-import { enrollmentValidationSchema } from './enrollment-validation-schema';
-import Label from '../ui/label';
-import SelectInput from '../ui/select-input';
-import ValidationError from '@/components/ui/form-validation-error';
 import { animateScroll } from 'react-scroll';
+import { useTranslation } from 'next-i18next';
+import { Control, FieldErrors, useForm } from 'react-hook-form';
+// types
+import { CourseOffering, Enrollment, Student } from '@/types';
+import { yupResolver } from '@hookform/resolvers/yup';
+// hooks
+import { useSettingsQuery } from '@/data/settings';
 import {
   useCreateEnrollmentMutation,
   useUpdateEnrollmentMutation,
 } from '@/data/enrollment';
 import { useStudentsQuery } from '@/data/student';
 import { useCourseOfferingsQuery } from '@/data/course-offering';
+// form-validation-schema
+import { enrollmentValidationSchema } from './enrollment-validation-schema';
+// components
+import Label from '@/components/ui/label';
+import Button from '@/components/ui/button';
+import Card from '@/components/common/card';
+import Description from '@/components/ui/description';
+import SelectInput from '@/components/ui/select-input';
+import StickyFooterPanel from '@/components/ui/sticky-footer-panel';
+import ValidationError from '@/components/ui/form-validation-error';
 
 function SelectCourseOffering({
   control,
@@ -30,14 +34,18 @@ function SelectCourseOffering({
   gradeLevel?: string;
 }) {
   const { t } = useTranslation();
-  const { courseOfferings, loading } = useCourseOfferingsQuery({ grade_level: gradeLevel });
+  const { courseOfferings, loading } = useCourseOfferingsQuery({
+    grade_level: gradeLevel,
+  });
   return (
     <div className="mb-5">
       <Label>{t('form:input-label-courses')}</Label>
       <SelectInput
         name="course_offering"
         control={control}
-        getOptionLabel={(option: any) => `${option.course.name} ${option.year} - ${option.grade_level.name} - batch ${option.batch}`}
+        getOptionLabel={(option: any) =>
+          `${option.course.name} ${option.year} - ${option.grade_level.name} - batch ${option.batch}`
+        }
         getOptionValue={(option: any) => option.id}
         options={courseOfferings!}
         isLoading={loading}
@@ -173,13 +181,17 @@ export default function CreateOrUpdateEnrollmentForm({
             initialValues
               ? t('form:item-description-edit')
               : t('form:item-description-add')
-          } ${t('form:category-description-helper-text')}`}
+          } ${t('form:enrollment-description-helper-text')}`}
           className="w-full px-0 pb-5 sm:w-4/12 sm:py-8 sm:pe-4 md:w-1/3 md:pe-5 "
         />
 
         <Card className="w-full sm:w-8/12 md:w-2/3">
           <SelectStudent control={control} errors={errors} />
-          <SelectCourseOffering control={control} errors={errors} gradeLevel={student?.current_grade?.name} />
+          <SelectCourseOffering
+            control={control}
+            errors={errors}
+            gradeLevel={student?.current_grade?.name}
+          />
         </Card>
       </div>
       <StickyFooterPanel className="z-0">
